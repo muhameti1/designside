221a197022945a9fe4e4c21287252d1143e2939a
muhameti1 <muhamethoxha123@gmail.com>
2025-01-24 21:45:01 +0100

    add hero component

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // Ensures static files are exported to the 'out' directory
   // Add any other Next.js config options here
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
